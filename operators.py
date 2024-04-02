489810d48645e8f3af15ba3df625158198eef575
BISWAS PARAJULI <letterstobisu@gmail.com>
2024-04-02 19:24:39 +0545

    Comparision Operator

--- operators.py
+++ operators.py
@@ -13,7 +13,7 @@ print("result of Multiple is ", result2)
 result3 = a / b
 print("result of Division is ", result3)
 # expo
-result4 = 2**5
+result4 = a**5
 print("Exponsial of data", result4)
 # modulas
 result5 = a % b
@@ -27,7 +27,34 @@ d = " Bhandari"
 result7 = c + d 
 print ("Sum of 2 Strings", result7)
 # Multiply String and Integer
-e = "Jolly Events and Entertainment"
+e = "Jolly Events and Entertainment "
 f = 5
 result8 = e * f
 print ("Multiply of Integer and String", result8)
+# Comparision operators
+# == equal to
+print ( 2 == 2 )
+print ( 2 == "2" )
+# != not equal to 
+print ( 44 != 33 )
+# > greater than
+print ( 20 > 30 )
+# < less than
+print ( 35 < 25 )
+# >= greater than equal to
+print ( 24 <= 12 ) 
+# <= less than equal to 
+print ( 35 >= 24 )
+# Logical Operators
+# and
+print ((77>=15) and (1==2))
+# or
+print ( ( 77 >= 15 ) or ( 1 == 2 ) )
+# not
+print ( not ( 77 >= 15 ) or ( 1==2 ))
+# is
+x = 77
+y = 4
+print (x is y)
+# is not
+print (x is not y)

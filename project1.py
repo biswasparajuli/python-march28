fc1b653c33661da22f0618ff52d2f0c0d61a96ff
BISWAS PARAJULI <letterstobisu@gmail.com>
2024-04-04 10:31:55 +0545

    Project1 and 2

---
+++ project1.py
@@ -0,0 +1,15 @@
+# Project about the Percentage
+# Percentage Slab for the project 100-80 (Distinction), 80-60 (First Division), 60-45 (second Division), 45-35 (Third Division) and Below 35 (fail)
+marks = 88
+if( marks <=100 and marks >=80 ):
+    print ("You have Secured Distinction, Congratulation")
+elif( marks <80 and marks >=60 ):
+    print ("You have secured First Divison")
+elif( marks <60 and marks >=45 ):
+    print ("You have secured Second Division")
+elif( marks <45 and marks >=35 ):
+    print ("You have secured Third Divison")
+elif( marks <35 and marks >=0 ):
+    print ("Sorry you are fail")
+else:
+    print ("Invalid Number Range")
---
+++ project2.py
@@ -0,0 +1,30 @@
+# Project about the interest after the divison secured
+# Slab for the project 100-80 (Distinction), with 100-90 "Study Prasadi Academy" and 80-90 "Study St. Xavier" 
+# 80-60 (First Division), with 80-70 "Study ABC Management" and 70-60 "Study BCD Management"
+# 60-45 (second Division), run business
+# 45-35 (Third Division), Foreign Employment 
+# Below 35 (fail), Study Again
+marks = 32
+if( marks <=100 and marks >=80 ):
+    print ("You have Secured Distinction, Congratulation")
+    if(marks <=100 and marks >=90):
+        print("Study Prasadi Academy")
+    else:
+        print("Study St. Xavier")
+elif( marks <80 and marks >=60 ):
+    print ("You have secured First Divison")
+    if(marks <=80 and marks >=70):
+        print("Study ABC Management")
+    else:
+        print("Study BCD Management")
+elif( marks <60 and marks >=45 ):
+    print ("You have secured Second Division")
+    print("Run Business")
+elif( marks <45 and marks >=35 ):
+    print ("You have secured Third Divison")
+    print("Try Foreign Employment")
+elif( marks <35 and marks >=0 ):
+    print ("Sorry you are fail")
+    print("Study Again")
+else:
+    print ("Invalid Number Range")

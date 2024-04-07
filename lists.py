c7d68971cdcabc9216582ea178085118bd644ee6
BISWAS PARAJULI <letterstobisu@gmail.com>
2024-04-07 18:51:49 +0545

    list

--- lists.py
+++ lists.py
@@ -10,4 +10,36 @@ print(a[:])
 print(a[::])
 # list in List
 data = [1,2,3,4,['Sudan', 'Test']]
-print(data[-2][0])
+print(data[4][0])
+# List Method
+# mutable
+# add data using append
+teachers = ['Nasir', 'Irfan' , 'Haris', 'Sheraz', 'Farah']
+teachers.append("Sudan")
+teachers.append(1)
+teachers.append(['1',3])
+print (teachers)
+# insert i.e. add to the given position
+teachers.insert(2, "Sudan")
+teachers.insert(-2, "Sudan")
+print (teachers)
+# extend means to extend list i.e. sum any 2 list
+data.extend(teachers)
+print (data)
+# delete list
+del teachers[0]
+print(teachers)
+# remove i.e. delete the data
+teachers.remove('Sudan')
+teachers.remove('Sudan')
+teachers.remove('Sudan')
+print (teachers)
+# pop i.e. it delete data that is at last
+colour=['Red','black', 'green']
+colour.pop()
+print( colour)
+# Clear
+colour.clear()
+print ("Test................", colour)
+
+

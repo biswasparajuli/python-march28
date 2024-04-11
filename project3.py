763d4f5a579e4e68f12c8b7f94a4c69faf758742
BISWAS PARAJULI <letterstobisu@gmail.com>
2024-04-11 12:05:48 +0545

    Project3

--- project3.py
+++ project3.py
@@ -1,8 +1,21 @@
+# Type 1 as you entered number your table will be printed
 a=[]
 for i in range(1,5):
-    n = int(input("Enter the number: "))
+    n = int(input(f'Enter the {i} number:'))
+    a.append(n)
+    for i in range(len(a)):
+        print(f'Multiplication table of {n}')
+        for j in range(1,11):
+            b = a[i]
+            print(f'{b} X {j} = {b * j}')
+
+# Type 2 as first you entered all number then your table will be printed once
+a=[]
+for i in range(1,5):
+    n = int(input(f'Enter the {i} number:'))
     a.append(n)
 for i in range(len(a)):
+    print(f'Multiplication table of {n}')
     for j in range(1,11):
         b = a[i]
         print(f'{b} X {j} = {b * j}')

b33c140928d70c4c08923f1e91019a04cbca6062
BISWAS PARAJULI <letterstobisu@gmail.com>
2024-04-24 18:54:03 +0545

    Games

--- games2_1.py
+++ games2_1.py
@@ -10,22 +10,23 @@ a = """
 
 print(a)
 
-random_number = random.randint(0, 2)
-print(random_number)
-n = int(input("enter a number "))
-
-while random_number != n and n < 3:
-    if (
+game_start = "Y"
+while game_start == "Y" or game_start == "y":
+    random_number = random.randint(0, 2)
+    # print(random_number)
+    n = int(input("Welcome Player, Enter Your Choice "))
+    if random_number == n:
+        print("It is tie bro")
+    elif n not in [0,1,2]:
+        print(
+            f"You have entered invalid number i.e. {n}. Please enter among 0, 1 or 2 "
+        )
+    elif (
         (random_number == 1 and n == 2)
         or (random_number == 0 and n == 1)
         or (random_number == 2 and n == 0)
     ):
         print("you won bro!!")
-        break
     else:
         print("You Lose")
-        n = int(input("Again enter a number: "))
-if random_number == n:
-    print("It is tie bro")
-elif n > 3:
-    print(f"You have entered invalid number i.e. {n}. Please enter among 0, 1 or 2 ")
+    game_start = input(f"Do you want to play again: (Y/N) ")
